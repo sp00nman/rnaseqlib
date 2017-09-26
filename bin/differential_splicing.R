ea63123bc4149b6641869e419c2049214e124949
Fiorella Schischlik <fiorella.schischlik@gmail.com>
2017-09-26 11:19:58 +0200

    added design formulas

--- bin/differential_splicing.R
+++ bin/differential_splicing.R
@@ -1,23 +1,30 @@
 ## internal information requires R/3.2.3 on cluster
-## Rscript --vanilla [file_name] [working_dir] [project_name] [meta_data] [num_cores]
+## Rscript --vanilla 
+# [this_script] 
+# [meta_data]
+# [exp_name]
+# [design_formulas]
+# [working_dir] 
+# [project_name] 
+# [num_cores]
 
 library(DEXSeq)
 library(BiocParallel)
+library(matrixStats)
 
 # get command line arguments
 args <- commandArgs(TRUE)
 print(args)
 
+meta_data <-args[1]
+exp_name <- args[2]
+design_formulas <- args[3]
+working_dir <- args[4]
+project_name <- args[5]
+num_cores <- as.numeric(args[6])
+
 # set working directory
-working_dir <- args[1]
 setwd(working_dir)
-
-# get project name
-project_name <- args[2]
-
-meta_data <-args[3]
-
-num_cores <- as.numeric(args[4])
 
 # load data
 countData <- read.csv(paste(working_dir, "/", project_name, "_sj_counts", sep=""), header=TRUE, row.names=1, sep="\t",check.names=F)
@@ -29,9 +36,6 @@ head(countData)
 # 20:49551774-49552684:-      52     126     149     136         108         99
 # 20:49552800-49557401:-      51     100     153     125         176        110
 # 20:49552800-49558567:-       4       2       0       2           8          6
-# 20:49557446-49558567:-       0       0       0       0           0          0
-# 20:49557471-49557641:-       2       2       7       4          15          0
-# 20:49557471-49557665:-       9      12       8      23          28          2
 
 featureIDs <- read.csv(
   paste(working_dir, "/", project_name, "_featureIDs", sep=""), 
@@ -44,9 +48,7 @@ head(featureIDs)
 # 1       1
 # 2       2
 # 3       3
-# 4       4
-# 5       5
-# 6       6
+
 groupIDs <- read.csv(
   paste(working_dir, "/", project_name, "_groupID", sep=""), 
   header=TRUE, sep="\t"
@@ -58,9 +60,6 @@ head(groupIDs)
 # 1 ENSG00000000419
 # 2 ENSG00000000419
 # 3 ENSG00000000419
-# 4 ENSG00000000419
-# 5 ENSG00000000419
-# 6 ENSG00000000419
 
 transcripts <- read.csv(
   paste(working_dir, "/", project_name, "_transcripts", sep=""), 
@@ -73,16 +72,37 @@ head(transcripts)
 # 1 0 20:49551774-49552684:-
 #   2 1 20:49552800-49557401:-
 #   3 2 20:49552800-49558567:-
-#   4 3 20:49557446-49558567:-
-#   5 4 20:49557471-49557641:-
-#   6 5 20:49557471-49557665:-
-#   
-sampleData <- read.csv(meta_data, header=TRUE, row.names=1, sep="\t")
+
+sampleData <- read.csv(
+  meta_data, 
+  header=TRUE, 
+  row.names=1, sep="\t")
+
 print("sampleData")
 dim(sampleData)
 head(sampleData)
 
-# some data transformation
+sampleData$bleeding_date <- as.Date(
+  as.character(sampleData$bleeding_date),
+  format="%d.%m.%Y")
+sampleData$extraction_date <- as.Date(
+  as.character(sampleData$extraction_date),
+  format="%d.%m.%Y")
+sampleData$date_of_birth <- as.Date(
+  as.character(sampleData$date_of_birth),
+  format="%d.%m.%Y")
+sampleData$storage_time <- sampleData$bleeding_date - sampleData$extraction_date
+sampleData$storage_time <- difftime(
+  sampleData$extraction_date, 
+  sampleData$bleeding_date, unit="days")
+sampleData$storage_time <- as.numeric(
+  sampleData$storage_time)
+sampleData$age <- difftime(
+  sampleData$bleeding_date, 
+  sampleData$date_of_birth, unit="weeks")
+sampleData$age <- as.numeric(sampleData$age)/52.14
+
+#some data transformation
 featureIDs$exon_ID <- paste("exon", featureIDs$exon_id, sep="")
 featureIDs <- as.vector(featureIDs$exon_ID)
 groupIDs <- as.vector(groupIDs[,])
@@ -91,7 +111,13 @@ transcripts <- as.vector(transcripts[,])
 
 # reorder sample data so that it has the same order as columns in countData
 sampleData.sorted <- sampleData[colnames(countData),]
-colnames(sampleData.sorted) <- c("condition", "batch_name", "disease_abbr", "genotype")
+
+# read in design formulas
+design <- read.csv(design_formulas)
+
+fitExpToVar <- design[design$exp_name==exp_name,]$fitExpToVar
+formulaFullModel <- design[design$exp_name==exp_name,]$formulaFullModel
+formulaReducedModel <- design[design$exp_name==exp_name,]$formulaReducedModel
 
 # print reformatted data
 print("Reformatted data:")
@@ -102,7 +128,11 @@ head(sampleData.sorted)
 
 ## DEXSeq workflow
 
+# assign number of cores
+BPPARAM=MulticoreParam(workers=num_cores)
+
 print("Creating DEXSeq data object:") 
+
 ECS = DEXSeqDataSet(
   countData=countData,
   sampleData=sampleData.sorted,
@@ -118,28 +148,57 @@ print("Estimating size factors:")
 ECS = estimateSizeFactors(ECS)
 
 print("Estimate Dispersion:")
-BPPARAM=MulticoreParam(workers=num_cores)
-ECS = estimateDispersions(ECS, BPPARAM=BPPARAM)
-#ECS = fitDispersionFunction(ECS)
+dxd_e1=estimateDispersions(
+  ECS,
+  formula=formulaFullModel,
+  BPPARAM=BPPARAM)
+
 print("Test for differential expression:")
-ECS = testForDEU(ECS, BPPARAM=BPPARAM)
+ECS = testForDEU(
+  ECS, 
+  reducedModel=formulaReducedModel,
+  fullModel=formulaFullModel,
+  BPPARAM=BPPARAM)
+
 print("Estimate exon fold changes:")
-ECS = estimateExonFoldChanges(ECS, fitExpToVar = "condition")
+ECS = estimateExonFoldChanges(ECS, fitExpToVar = fitExpToVar)
 
 print("Save R session:")
-save.image(paste(paste(working_dir, "/", project_name, ".RData", sep="")))
+save.image(paste(paste(working_dir, "/", 
+                       project_name, "_", 
+                       exp_name, ".RData", sep="")))
 
 print("Export results to file:")
-counts <- counts(ECS, normalized=FALSE)
-write.csv(counts, paste(working_dir, "/", project_name, "_counts", sep=""), sep="\t")
+dexseq_counts <-featureCounts(
+  ECS, 
+  normalized=FALSE)
+write.table(
+  counts, 
+  file=paste(working_dir, "/", 
+             project_name, "_",
+             exp_name, "_counts", sep=""),
+  quote=FALSE,
+  col.names=TRUE,
+  sep="\t")
 
-normalized_counts <- counts(ECS, normalized=TRUE)
-write.csv(normalized_counts, paste(working_dir, "/", project_name, "_counts_normalized", sep=""), sep="\t")
+dexseq_counts <-featureCounts(
+  ECS, 
+  normalized=TRUE)
+write.table(
+  normalized_counts, 
+  file=paste(working_dir, "/", 
+             project_name, "_",
+             exp_name, "_counts_normalized", sep=""),
+  quote=FALSE,
+  col.names=TRUE,
+  sep="\t")
 
 dexseq_results <- DEXSeqResults(ECS)
-write.csv(dexseq_results, paste(working_dir, "/", project_name, "_dexseq_results", sep=""), sep="\t")
-
-## TODO
-# vst function from DESeq2
-# MDS
-
+write.table(
+  dexseq_results,
+  paste(working_dir, "/", 
+        project_name, "_",
+        exp_name, "_dexseq_results", sep=""),
+  quote=FALSE,
+  col.names=TRUE,
+  sep="\t")
